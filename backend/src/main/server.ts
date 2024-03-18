d2f17b10638a145d23a999702c1e1879fcecefa0
Bruno Rocha <brunorocha2674@gmail.com>
2024-03-18 12:44:05 -0300

    init

---
+++ backend/src/main/server.ts
@@ -0,0 +1,22 @@
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './modules/app.module.js'
+
+const port = 3000
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule)
+
+  app.enableCors()
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      always: true,
+    })
+  )
+
+  await app.listen(port)
+  process.stdout.write(`🚀 Server is running on ${await app.getUrl()}\n`)
+}
+
+bootstrap().catch(console.error)
